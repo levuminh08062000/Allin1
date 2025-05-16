593378cac13e15b85115adf052d10875e0fd81cf
levuminh08062000 <113541947+levuminh08062000@users.noreply.github.com>
2025-05-16 17:27:01 +0700

    m

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default () => {
   return defineConfig({
-    base: "",
+    base: "./",
     plugins: [zaloMiniApp(), react()],
     build: {
       assetsInlineLimit: 0,
